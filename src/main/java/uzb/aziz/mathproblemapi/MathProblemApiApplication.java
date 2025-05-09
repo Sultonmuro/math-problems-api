12e9640349f13c61849ed4a574dd10a5d043a22d
Sultonmuro <uwerplaystation@gmail.com>
2025-05-09 17:00:57 +0500

    Just created my another pet-project

---
+++ src/main/java/uzb/aziz/mathproblemapi/MathProblemApiApplication.java
@@ -0,0 +1,13 @@
+package uzb.aziz.mathproblemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathProblemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathProblemApiApplication.class, args);
+    }
+
+}
